4370645dfd8db20906ac5402903ad5a607143b55
macbeth <layne.wegenast@gmail.com>
2022-07-11 19:20:10 -0500

    took out login bootstrap

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -40,14 +40,14 @@ const Login = (props) => {
   };
 
   return (
-    <main className="flex-row justify-center mb-4">
-      <div className="col-12 col-md-6">
-        <div className="card">
-          <h4 className="card-header">Login</h4>
-          <div className="card-body">
+    <main >
+      <div >
+        <div >
+          <h4 >Login</h4>
+          <div >
             <form onSubmit={handleFormSubmit}>
               <input
-                className="form-input"
+              
                 placeholder="Your email"
                 name="email"
                 type="email"
@@ -56,7 +56,7 @@ const Login = (props) => {
                 onChange={handleChange}
               />
               <input
-                className="form-input"
+                
                 placeholder="******"
                 name="password"
                 type="password"
@@ -64,7 +64,7 @@ const Login = (props) => {
                 value={formState.password}
                 onChange={handleChange}
               />
-              <button className="btn d-block w-100" type="submit">
+              <button  type="submit">
                 Submit
               </button>
             </form>
